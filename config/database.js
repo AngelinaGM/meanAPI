1f9bd15b9bbca04c4e896153bbcc59b864a64720
Angelina Gorbachevskaya <amgorbachevskaya@gmail.com>
2017-09-04 09:57:26 +0300

    I've done API part

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // database: 'mongodb://admin:admin@cluster0-shard-00-00-kxhd3.mongodb.net:27017,cluster0-shard-00-01-kxhd3.mongodb.net:27017,cluster0-shard-00-02-kxhd3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+    database: 'mongodb://localhost:27017/meanauth',
+    secret: 'yoursecret'
+}
